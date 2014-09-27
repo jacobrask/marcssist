5ad223b12f757781d7ef4d00925cc4be216d64a6
Jacob Rask <jacob@jacobrask.net>
2014-09-27 21:12:29 +0200

    Improve inline docs and move around some code

--- marcssist.js
+++ marcssist.js
@@ -2,13 +2,19 @@
 "use strict";
 
 
-// Global counter to keep class names unique
+/**
+ * Global id to keep class names unique.
+ * @type {number}
+ */
+
 var classId = 0;
+
 
 /**
  * @param {object} options
  * @param {object} options.prefix=true Add vendor prefixes
  */
+
 function Marcssist(options) {
 
   // Don't require |new|
@@ -23,141 +29,216 @@ function Marcssist(options) {
    * @param {string} selector= Optional prefix for class name.
    * @returns {string}
    */
-  function insert(style, selector) {
+  this.add = function (style, selector) {
     var className = "mx__"+(classId++);
     selector = (selector ? selector+" ." : ".") + className;
-    var declarations = processStyles(selector, style);
+    var rules = rulesFromStyle(selector, style);
     if (options == null || options.prefix !== false) {
-      declarations.forEach(function(decl) {
-        addPrefixes(decl.style);
+      rules.forEach(function(set) {
+        addPrefixes(set.style);
       });
     }
-    insertRules(declarations);
+    insertRules(rules);
     return className;
-  }
+  };
+}
 
-
-  /**
-   * Flatten all styles to objects with selector/style pairs.
-   *
-   * @param {selector} obj
-   * @param {object} obj
-   * @returns {array}
-   */
-
-  function processStyles(sel, obj) {
-    var prop, value, style = {}, styles = [];
-    for (prop in obj) {
-      value = obj[prop];
-      // found an object, recurse with property as added selector
-      if (isPlainObject(value)) {
-        styles = styles.concat(
-          processStyles(sel + (prop[0] === ":" ? prop : " "+prop), value)
-        );
-      } else {
-        style[hyphenate(prop)] = value;
-      }
-    }
-    styles.push({
-      selector: sel,
-      style: style
-    });
-    return styles;
-  }
-
-
-  var sheet = null;
-  function createStyleSheet() {
-    var ss = document.createElement("style");
-    document.head.appendChild(ss);
-    sheet = ss.sheet;
-  }
-
-  function insertRules(rules) {
-    if (sheet === null) createStyleSheet();
-    rules.forEach(function(rule) {
-      var pairs = [], prop;
-      for (prop in rule.style) {
-        pairs.push(prop + ":" + rule.style[prop]);
-      }
-      console.log(pairs);
-      sheet.insertRule(rule.selector + "{" + pairs.join(";") + "}", 0);
-    });
-  }
-
-  function hyphenate(str) {
-    return str.replace(/[A-Z]/g, function($0) { return '-'+$0.toLowerCase() });
-  }
-
-  function isPlainObject(obj) {
-    // If the object has a custom toString method, it's probably intended to
-    // be expanded to that rather than to child selectors
-    return obj === Object(obj)
-      && Object.prototype.toString === obj.toString;
-  }
-
-
-  function addPrefixes(style) {
-    var value, prop, pfxProp, pfxVal;
-    for (prop in style) {
-      value = style[prop];
-      prop = prefixProp(prop) || prop;
-      value = prefixValue(value, prop) || value;
-      style[prop] = value;
-    }
-    return style;
-  }
-
-  var prefixedProps = {};
-  var prefix = null;
-  function updatePrefixedProps() {
-    var prefixRe = /^(-[a-z]+-)(.*)/;
-    var style = window.getComputedStyle(document.documentElement);
-    for (var i = 0, l = style.length, match; i < l; i++) {
-      if ((match = style[i].match(prefixRe)) && !(match[2] in style)) {
-        prefix || (prefix = match[1]);
-        prefixedProps[match[2]] = true;
-      }
-    }
-  }
-
-  function prefixProp(prop) {
-    if (prefix === null) updatePrefixedProps();
-    if (prefixedProps[prop]) {
-      return prefix + prop;
-    }
-  }
-
-  var prefixedValues = {};
-  var style = document.createElement("div").style;
-  function prefixValue(value, prop) {
-    if (prefix === null) updatePrefixedProps();
-    // Cached
-    if (prefixedValues[prop] === value) {
-      return prefix + value;
-    }
-    style[prop] = "";
-    style[prop] = value;
-    // Supported
-    if (!!style[prop]) return;
-    style[prop] = prefix+value;
-    // Supported with prefix
-    if (!!style[prop]) {
-      prefixedValues[prop] = value;
-      return prefix + value;
-    }
-  }
-
-  this.insert = insert;
-};
 
 var marcssist = Marcssist;
-marcssist.insert = Marcssist().insert;
+marcssist.add = new Marcssist().add;
 
 if (typeof module !== "undefined" && module.exports) {
   module.exports = marcssist;
 } else {
   window.marcssist = marcssist;
 }
+
+
+/**
+ * Prefix detected when updating |prefixedProps|.
+ * @type {string?}
+ */
+
+var prefix = null;
+
+
+/**
+ * Lookup map of properties that should be prefixed.
+ * @type {object}
+ */
+
+var prefixedProps = {};
+
+function updatePrefixedProps() {
+  var prefixRe = /^(-[a-z]+-)(.*)/;
+  var style = window.getComputedStyle(document.documentElement);
+  for (var i = 0, l = style.length, match; i < l; i++) {
+    if ((match = style[i].match(prefixRe)) && !(match[2] in style)) {
+      prefix || (prefix = match[1]);
+      prefixedProps[match[2]] = true;
+    }
+  }
+}
+
+
+/**
+ * Lookup map of property/value pairs that should be prefixed.
+ * @type {object}
+ */
+
+var prefixedValues = {};
+
+
+/**
+ * Style object used to test if a property/value pair is supported.
+ * @type {object}
+ */
+
+var dummyStyle = document.createElement("div").style;
+
+
+/**
+ * Style element, used for all instances.
+ * @type {Element?}
+ */
+
+var sheet = null;
+
+function createStyleSheet() {
+  var ss = document.createElement("style");
+  document.head.appendChild(ss);
+  sheet = ss.sheet;
+}
+
+
+/**
+ * Flattens any nested selector combinators, returning CSS rule sets
+ * represented by selector/style pairs.
+ *
+ * @param {string} selector Base CSS selector
+ * @param {object} block CSS property/values declaration block
+ * @returns {array.object}
+ */
+
+function rulesFromStyle(selector, block) {
+  var prop, value, style = {}, rules = [];
+  for (prop in block) {
+    value = block[prop];
+    // Found an object, recurse with property added to selector.
+    if (isPlainObject(value)) {
+      rules = rules.concat(
+        rulesFromStyle(combineSelectors(selector, prop), value)
+      );
+    } else {
+      if (prop === "content") value = "'"+value+"'";
+      style[hyphenate(prop)] = value;
+    }
+  }
+  rules.push({
+    selector: selector,
+    style: style
+  });
+  return rules;
+}
+
+
+/**
+ * Add rule sets to stylesheet.
+ *
+ * @param {array} rules
+ */
+
+function insertRules(rules) {
+  if (sheet === null) createStyleSheet();
+  rules.forEach(function(rule) {
+    var pairs = [], prop;
+    for (prop in rule.style) {
+      pairs.push(prop + ":" + rule.style[prop]);
+    }
+    sheet.insertRule(rule.selector + "{" + pairs.join(";") + "}", 0);
+  });
+}
+
+
+/**
+ * Pseudo classes/elements and attribute selectors should immediately
+ * follow the previous selector, others should be space separated.
+ *
+ * @param {string} a
+ * @param {string} b
+ * @returns {string} "a b" or "ab"
+ */
+
+function combineSelectors(a, b) {
+  var separator = /^[:\[]/.test(b) ? "" : " ";
+  return a + separator + b;
+}
+
+
+/**
+ * Add vendor prefixes to style block.
+ *
+ * @param {object} style
+ * @returns {object} same object as argument
+ */
+
+function addPrefixes(style) {
+  var value, prop;
+  for (prop in style) {
+    value = style[prop];
+    prop = prefixProp(prop) || prop;
+    value = prefixValue(value, prop) || value;
+    style[prop] = value;
+  }
+  return style;
+}
+
+
+function prefixProp(prop) {
+  if (prefix === null) updatePrefixedProps();
+  if (prefixedProps[prop]) {
+    return prefix + prop;
+  }
+}
+
+
+function prefixValue(value, prop) {
+  if (prefix === null) updatePrefixedProps();
+  // Cached
+  if (prefixedValues[prop] === value) {
+    return prefix + value;
+  }
+  dummyStyle[prop] = "";
+  dummyStyle[prop] = value;
+  // Supported
+  if (!!dummyStyle[prop]) return;
+  dummyStyle[prop] = prefix+value;
+  // Supported with prefix
+  if (!!dummyStyle[prop]) {
+    prefixedValues[prop] = value;
+    return prefix + value;
+  }
+}
+
+
+function hyphenate(str) {
+  return str.replace(/[A-Z]/g, function($0) { return '-'+$0.toLowerCase(); });
+}
+
+
+/**
+ * Check if an object is an object, and has the original toString method.
+ * If toString is changed, the caller most likely wants the string value
+ * and not the object properties.
+ *
+ * @param {*} obj
+ * @returns {boolean}
+ */
+
+function isPlainObject(obj) {
+  return obj === Object(obj)
+      && Object.prototype.toString === obj.toString;
+}
+
 
 })();
